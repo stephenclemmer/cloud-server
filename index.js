2d7df739144286626f85242a9414ee12341a6ef1
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-10-10 19:29:39 -0700

    deployed the server through the GUI an CLI. Wrote Readme documenting the process for both deployments.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3002;
 
 app.get('/', (req, res, next) => {
   res.status(200).send('Hello World! Welcome to this simple server!');
-})
+});
 
 app.get('/hello', (req, res, next) => {
   let { name } = req.query;
